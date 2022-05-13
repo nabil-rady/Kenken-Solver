42214e3fa696863c116206262cf4485a77e521e8
nabil-rady <midorady9999@gmail.com>
2022-05-14 01:28:46 +0200

    generate domains for kenken

--- kenken.py
+++ kenken.py
@@ -1,5 +1,6 @@
 from functools import reduce
 import random
+from itertools import product, permutations
 
 def operation(operator):
     if operator == '+':
@@ -38,20 +39,20 @@ def generate(board_size):
     # This will contain all uncaged cells
     uncaged = sorted(board.keys(), key=lambda x: x[1])
 
-    cliques = []
+    cages = []
     while uncaged:
 
-        cliques.append([])
+        cages.append([])
 
-        clique_size = random.randint(1, 4)
+        cage_size = random.randint(1, 4)
 
         cell = uncaged[0]
 
         uncaged.remove(cell)
 
-        cliques[-1].append(cell)
+        cages[-1].append(cell)
 
-        for _ in range(clique_size - 1):
+        for _ in range(cage_size - 1):
 
             adjs = [adj for adj in uncaged if is_adjacent(cell, adj)]
 
@@ -62,17 +63,17 @@ def generate(board_size):
 
             uncaged.remove(cell)
             
-            cliques[-1].append(cell)
+            cages[-1].append(cell)
             
-        clique_size = len(cliques[-1])
+        cage_size = len(cages[-1])
         
-        if clique_size == 1:
-            cell = cliques[-1][0]
-            cliques[-1] = ((cell, ), '.', board[cell])
+        if cage_size == 1:
+            cell = cages[-1][0]
+            cages[-1] = ((cell, ), '.', board[cell])
             continue
         
-        elif clique_size == 2:
-            first, second = cliques[-1][0], cliques[-1][1]
+        elif cage_size == 2:
+            first, second = cages[-1][0], cages[-1][1]
             if board[first] / board[second] > 0 and not board[first] % board[second]:
                 operator = "/" 
             else:
@@ -81,8 +82,44 @@ def generate(board_size):
         else:
             operator = random.choice("+*")
 
-        clique_val = reduce(operation(operator), [board[cell] for cell in cliques[-1]])
+        cage_val = reduce(operation(operator), [board[cell] for cell in cages[-1]])
 
-        cliques[-1] = (((cliques[-1]), operator, int(clique_val)))
+        cages[-1] = (tuple(cages[-1]), operator, int(cage_val))
 
-    return cliques
+    return cages
+
+def row_or_col_same(cell_1, cell_2):
+    x1, y1 = cell_1
+    x2, y2 = cell_2
+
+    same_row = x1 == x2
+    same_col = y1 == y2
+
+    return same_row or same_col
+
+# True if cage values are in conflict.
+def cage_is_conflicting(cage, cell_values):
+    for (cell1, cell_value1) in zip(cage, cell_values):
+        for(cell2, cell_value2) in zip(cage ,cell_values):
+            if row_or_col_same(cell1, cell2) and cell1 != cell2 and cell_value1 == cell_value2: # If 1) there is a common col or row 2) not the same cell (because of foor loop) 3) same number, then this solution for the cage is wrong.
+                return True    
+    return False
+
+# True if kenken cage value is correct.
+def satisfies_cage_value(values, operation, cage_value):
+    return any([reduce(operation, permutation) == cage_value for permutation in permutations(values)])
+
+# Generates possible solutions.
+def generate_domains(size, cages):
+    domains = {}
+    for cage in cages:
+        cage_cells, operator, cage_value = cage
+
+        domains[cage_cells] = list(product(range(1, size + 1), repeat=len(cage_cells)))
+
+        qualifies = lambda values: not cage_is_conflicting(cage_cells, values) and satisfies_cage_value(values, operation(operator), cage_value)
+
+        domains[cage_cells] = list(filter(qualifies, domains[cage_cells]))
+
+    return domains
+
